eeb79e0d23a3f6b773220ac6a80a3734b18605ba
Jaeseong Yoo <praster1@gmail.com>
2022-05-03 08:02:28 +0900

    bug fix

---
+++ javascripts/_index/ajax_notice_20220503_0802.js
@@ -0,0 +1,27 @@
+function ajax_notice(inputUUID, tableName)
+{
+	url = "/get_index_notice/" + inputUUID + "/"
+	
+	$.ajax({
+		url : url,
+		success:function(data){
+			var t1 = $(tableName).DataTable();
+			
+			for(key in data){
+				html = '<tr>'
+				html += '<td style="font-size:small;">'
+				html += '<a href="/intro_notice/readArticle/' + data[key].uuid + '/">'
+				html += '<span style="color:black">' + textLengthOverCut(data[key].title, 100, " ...") + '</span>'
+				html += '</a>'
+				html += '</td>'
+				html += '<td style="font-size:small;">'
+				html += '<span style="color:black">' + data[key].created_at + '</span>'
+				html += '</td>'
+				html += '</tr>'
+				
+				const temp = $(html)
+				t1.row.add(temp).draw();
+			}				
+		}
+	})
+}
