ee9d839cee2175c3885fa3bcaf2e905e80d767c1
Jaeseong Yoo <praster1@gmail.com>
2022-05-03 07:00:55 +0900

    community - statistics

---
+++ javascripts/_comm/community/statistics/table_community_active_writer.js
@@ -0,0 +1,40 @@
+function table_community_active_writer()
+{
+	var table = $('#table_community_active_writer').DataTable( {
+		"responsive": true,
+		"processing": true,
+		"serverSide": true,
+		"ordering": false,
+		"responsive": true,
+		"autoWidth": true,
+		"paging": true,
+		"pagingType": "simple",
+		"lengthChange": false,
+		"pageLength": 5,
+		"searching":false,
+		"ajax": {
+			"url": "/get_community_contents_active_writer/",
+			"type": "GET",
+			"dataSrc": function(res) {
+				var data = res.data;
+				return data;
+			}
+		},
+		"columns" : [
+			{"data": "Username"},
+			{"data": "Content"}
+		],
+		columnDefs: [
+			{	targets: 0,	className: 'dt-body-center'	},
+			{	targets: 1,	className: 'dt-body-right'	}
+		],
+		"oLanguage": { "sInfo" : "" },
+		"language": { "lengthMenu": "" }
+	} );
+	
+	table.on( 'responsive-resize', function ( e, datatable, columns ) {
+		var count = columns.reduce( function (a,b) {
+			return b === false ? a+1 : a;
+		}, 0 );
+	} );
+}
---
+++ javascripts/_comm/community/statistics/table_community_famous_writer.js
@@ -0,0 +1,43 @@
+function table_community_famous_writer(getValue)
+{
+	var tableName = '#table_community_famous_writer_' + getValue
+	var url = "/get_community_contents_famous_writer/" + getValue + "/sum/"
+	
+	var table = $(tableName).DataTable( {
+		"responsive": true,
+		"processing": true,
+		"serverSide": true,
+		"ordering": false,
+		"responsive": true,
+		"autoWidth": true,
+		"paging": true,
+		"pagingType": "simple",
+		"lengthChange": false,
+		"pageLength": 5,
+		"searching":false,
+		"ajax": {
+			"url": url,
+			"type": "GET",
+			"dataSrc": function(res) {
+				var data = res.data;
+				return data;
+			}
+		},
+		"columns" : [
+			{"data": "Username"},
+			{"data": "Content"}
+		],
+		columnDefs: [
+			{	targets: 0,	className: 'dt-body-center'	},
+			{	targets: 1,	className: 'dt-body-right'	}
+		],
+		"oLanguage": { "sInfo" : "" },
+		"language": { "lengthMenu": "" }
+	} );
+	
+	table.on( 'responsive-resize', function ( e, datatable, columns ) {
+		var count = columns.reduce( function (a,b) {
+			return b === false ? a+1 : a;
+		}, 0 );
+	} );
+}
