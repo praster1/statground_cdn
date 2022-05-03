816ebcc5d37f1820dc0b3ab738d69dd48d1a0389
Jaeseong Yoo <praster1@gmail.com>
2022-05-04 02:06:08 +0900

    members

---
+++ javascripts/dashboard/members/ajax_members_table.js
@@ -0,0 +1,26 @@
+function ajax_members_table()
+{
+	$("#members_list_table").hide()
+	
+	$.ajax({
+		url : "/get_memberList/",
+		success:function(data){
+			var t1 = $('#members_list_table').DataTable();
+			
+			for(key in data){
+				html = "<tr>"
+				html += "<td style='font-size:small'>" + data[key].email.toString() + "</td>"
+				html += "<td style='font-size:small'><b>" + textLengthOverCut(data[key].username.toString(), 10) + "</b></td>"
+				html += "<td style='font-size:small'>" + data[key].date_last_login.toString() + "</td>"
+				html += "<td style='font-size:small'>" + data[key].date_joined.toString() + "</td>"
+				html += "</tr>"
+				
+				const temp = $(html)
+				t1.row.add(temp).draw();
+			}
+			
+			$("#members_list_table").show()
+			$(".trigger_members_list_table").hide()
+		}
+	})
+}
