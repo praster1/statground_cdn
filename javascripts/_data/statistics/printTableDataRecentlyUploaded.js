41b4360497628dd93b51f144dcfefe17b337904e
Jaeseong Yoo <praster1@gmail.com>
2022-05-03 20:35:10 +0900

    data - printTableDataRecentlyUploaded

---
+++ javascripts/_data/statistics/printTableDataRecentlyUploaded.js
@@ -0,0 +1,40 @@
+function printTableDataRecentlyUploaded()
+{		
+	var url = "/get_data_recently_uploaded/"
+
+	var table = $('#board_data_recently_uploaded').DataTable( {
+		"destroy": true,
+		"responsive": true,
+		"processing": true,
+		"serverSide": true,
+		"ordering": false,
+		"responsive": true,
+		"autoWidth": true,
+		"paging": false,
+		"pageLength": 5,
+		"lengthChange": false,
+		"searching": false,
+		"ajax": {
+			"url": url,
+			"type": "GET",
+			"dataSrc": function(res) {
+				var data = res.data;
+				return data;
+			}
+		},
+		"columns" : [
+			{"data": "Dataset"}
+		],
+		"oLanguage": { "sInfo" : "" },
+		"language": { "lengthMenu": "" },
+		"drawCallback": function( settings ) {
+			 $("#board_data_recently_uploaded thead").remove();
+		 }
+	} );
+	
+	table.on( 'responsive-resize', function ( e, datatable, columns ) {
+		var count = columns.reduce( function (a,b) {
+			return b === false ? a+1 : a;
+		}, 0 );
+	} );
+};
