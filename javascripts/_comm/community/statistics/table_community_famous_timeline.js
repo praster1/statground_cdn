5421f5817817953eea732f87ab35f12c6070f7f5
Jaeseong Yoo <praster1@gmail.com>
2022-05-03 20:27:49 +0900

    bug fix

--- javascripts/_comm/community/statistics/table_community_famous_timeline.js
+++ javascripts/_comm/community/statistics/table_community_famous_timeline.js
@@ -1,5 +1,7 @@
 function table_community_famous_timeline(getValue)
 {
+	getValue = getValue.replace("_", "")
+	
 	var tableName = '#table_community_famous_timeline_' + getValue
 	var url = "/get_community_contents_famous_timeline/" + getValue + "/"
 	
