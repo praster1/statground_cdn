94f077002f0bbb59d509795b10798f6c38ae72fc
Jaeseong Yoo <praster1@gmail.com>
2022-05-24 05:36:52 +0900

    bug fix

---
+++ javascripts/_defaults/setSignInHeader_202205240536.js
@@ -0,0 +1,31 @@
+function setSignInHeader() {
+	$.ajax({
+		url : "/get_user_info/",
+		success:function(data){		
+			$("#userinfo_role").html("")
+			
+			html = data.group_info.group_name.toString() + " 계정입니다."
+			$("#userinfo_role").append(html)
+
+
+			// 셀 사용량
+			$("#userinfo_limitcellsize").html("")
+			html = setUserCellUsage(data, "rate") 
+			$("#userinfo_limitcellsize").append(html)
+
+
+			// 파일 저장 용량
+			$("#userinfo_limitfilesize").html("")
+			html = setUserFileStorage(data, "rate") 
+			$("#userinfo_limitfilesize").append(html)
+			
+			if ("is_admin" in data.group_info || "is_staff" in data.group_info )
+			{
+				 if (data.group_info.is_admin > 0 || data.group_info.is_staff > 0)
+				{
+					$("#notification_operation").show()
+				}
+			}
+		}
+	})
+}
