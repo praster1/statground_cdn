79086e9d601df11b301e5efea0e29a3214b7ca3f
Jaeseong Yoo <praster1@gmail.com>
2022-05-24 05:31:56 +0900

    usage

---
+++ javascripts/_defaults/setUserFileStorage_202205240524.js
@@ -0,0 +1,60 @@
+function setUserFileStorage(data, type="full") 
+{
+	html = ""
+
+	var uploadedFileSize = data.log_info.used_file_size.toFixed(3)		// 현재까지 업로드한 파일 크기
+	var limitFileSize = data.group_info.limit_file_size		// 업로드 한계 크기
+	
+	if (type == "full")
+	{
+		if (userinfo_limitfilesize > 0)
+		{
+			limitFileSize = data.group_info.limit_file_size.toFixed(3)
+		}
+
+		if (limitFileSize == 0) {
+			html = '<center><p style="color:black;">' + numberWithCommas(uploadedFileSize) + ' MB / 0.00 MB' + '</p></center>'
+			
+		} else if (limitFileSize == -1) {
+			html = '<div class="progress-bar progress-bar-info" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%;">'
+			html += '<center><p style="color:black;">' + numberWithCommas(uploadedFileSize) + ' MB / ∞ MB' + '</p></center>'
+			html += '</div>'
+			
+		} else {
+			var usageRateFileSize = numberWithCommas((uploadedFileSize / limitFileSize * 100).toFixed(3))
+			var usageRateFileSize_rate = numberWithCommas(((uploadedFileSize / limitFileSize * 100)).toFixed(3))
+			var usageRateFileSize_reverse = numberWithCommas((100 - (uploadedFileSize / limitFileSize * 100)).toFixed(3))
+			html = '<div class="progress-bar progress-bar-info" role="progressbar" aria-valuenow="' + usageRateFileSize_reverse + '" aria-valuemin="0" aria-valuemax="100" style="width: ' + usageRateFileSize_reverse + '%;">'
+			html += '<center><p style="color:black;">' + usageRateFileSize + 'MB / ' + usageRateFileSize + 'MB' + '(' + usageRateFileSize_rate + '% 사용중)</p></center>'
+			html += '</div>'
+		}
+	}
+	else
+	{
+		var uploadedFileSize = data.log_info.used_file_size.toFixed(2)		// 현재까지 업로드한 파일 크기
+		var limitFileSize = data.group_info.limit_file_size		// 업로드 한계 크기
+		
+		if (userinfo_limitfilesize > 0)
+		{
+			limitFileSize = data.group_info.limit_file_size.toFixed(2)
+		}
+
+		if (limitFileSize == 0) {
+			html = '<center><p style="color:black;">' + numberWithCommas(uploadedFileSize) + ' MB / 0.00 MB' + '</p></center>'
+			
+		} else if (limitFileSize == -1) {
+			html = '<div class="progress-bar progress-bar-info" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%;">'
+			html += '<center><p style="color:black;">' + numberWithCommas(uploadedFileSize) + ' MB 사용중</p></center>'
+			html += '</div>'
+			
+		} else {
+			var usageRateFileSize = numberWithCommas((uploadedFileSize / limitFileSize * 100).toFixed(2))
+			var usageRateFileSize_reverse = numberWithCommas((100 - (uploadedFileSize / limitFileSize * 100)).toFixed(2))
+			html = '<div class="progress-bar progress-bar-info" role="progressbar" aria-valuenow="' + usageRateFileSize_reverse + '" aria-valuemin="0" aria-valuemax="100" style="width: ' + usageRateFileSize_reverse + '%;">'
+			html += '<center><p style="color:black;">' + usageRateFileSize + '% 사용중</p></center>'
+			html += '</div>'
+		}
+	}
+	
+	return html
+}
