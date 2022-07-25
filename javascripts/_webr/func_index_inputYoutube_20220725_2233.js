3f5b1e370a5d15893ab24e1372590b60e5565253
Jaeseong Yoo <praster1@gmail.com>
2022-07-25 22:35:44 +0900

    _20220725_2233

---
+++ javascripts/_webr/func_index_inputYoutube_20220725_2233.js
@@ -0,0 +1,22 @@
+function func_index_inputYoutube(channelUUID) {
+	var url = "/get_webr_index_youtube/?uuid=" + channelUUID + "&videoCount=1"
+	
+	$.ajax({
+		url : url,
+		success:function(data){
+			for(key in data){
+				$("#index_youtube").html("")
+				
+				html = '<center>'
+				html += '<a class="thumb-image" href="' + data[key].url + '" target="_blank">'
+				html += '<img src="' + data[key].url_thumbnail + '" class="img-responsive" alt="' + textLengthOverCut(data[key].title, 15) + '">'
+				html += '</a>'
+				
+				html += '<h5 class="mg-title text-semibold" style="color:black;">' + data[key].title + '</h5>'
+				html += '</center>'
+				
+				$("#index_youtube").append(html)
+			}
+		}
+	})
+}
