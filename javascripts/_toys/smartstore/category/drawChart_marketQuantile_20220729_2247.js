4044f755f12f78674b590e061f892f5466da4451
Jaeseong Yoo <praster1@gmail.com>
2022-07-29 22:49:53 +0900

    drawChart_marketQuantile_20220729_2247

---
+++ javascripts/_toys/smartstore/category/drawChart_marketQuantile_20220729_2247.js
@@ -0,0 +1,38 @@
+function drawChart_marketQuantile(inputData, divID, columnName, inputTitle = "제목")
+{
+	$("#" + divID).hide()
+			
+	google.charts.load("current", {packages:['corechart']});
+	google.charts.setOnLoadCallback(drawChart);
+
+	function drawChart() {
+		var chartData = new google.visualization.DataTable();
+		chartData.addColumn('string', '백분율');
+		chartData.addColumn('number', columnName);
+
+		for(key in inputData){ chartData.addRows([ [	key + "%", nullConverter(inputData[key]) ] ]);
+		}
+
+		var options = {
+			title: inputTitle,
+			width: '100%',
+			legend: { position: 'bottom' },
+			crosshair: {orientation: 'vertical', trigger: 'focus'},
+			focusTarget: 'category',
+			legend: { position: "none" },
+			hAxis: { textStyle: { color: 'green' }, format: 'yy-MM-dd' },
+			colors: ['#ec8f6e'],
+			explorer: { actions: ['dragToZoom', 'rightClickToReset'], axis: 'horizontal' }
+		};
+
+		var chart = new google.visualization.ColumnChart(document.getElementById(divID));
+		chart.draw(chartData, options);
+		
+		$(window).smartresize(function () {
+			chart.draw(chartData, options);
+		});
+
+	}
+	
+	$("#" + divID).show()
+}
