9fb74781c356b193ee0e59e1f7fb48824376b227
Jaeseong Yoo <praster1@gmail.com>
2022-07-29 22:48:13 +0900

    ajax_get_category_content_keyword_list_table_20220729_2247

---
+++ javascripts/_toys/smartstore/category/ajax_get_category_content_keyword_list_table_20220729_2247.js
@@ -0,0 +1,38 @@
+function ajax_get_category_content_keyword_list_table(categoryCode)
+{
+	url = "/smartstore/getSmartstoreCategoryContent_keywordList_table/" + categoryCode.toString() + "/"
+	
+	var table = $('#table_keywordList').DataTable( {
+		"destroy": true,
+		"responsive": true,
+		"processing": true,
+		"serverSide": true,
+		"ordering": false,
+		"responsive": true,
+		"autoWidth": true,
+		"paging": true,
+		"searching": false,
+		"lengthChange": false,
+		"pageLength": 7,
+		"ajax": {
+			"url": url,
+			"type": "GET",
+			"dataSrc": function(res) {
+				var data = res.data;
+				return data;
+			}
+		},
+		"columns" : [
+			{"data": "Keyword"}
+		],
+		"oLanguage": { "sInfo" : "" },
+		"language": { "lengthMenu": "",
+						   "processing": "<img src='https://cdn.jsdelivr.net/gh/praster1/statground_cdn/images/tati/9.png' width='35%'/>" }
+	} );
+	 
+	table.on( 'responsive-resize', function ( e, datatable, columns ) {
+		var count = columns.reduce( function (a,b) {
+			return b === false ? a+1 : a;
+		}, 0 );
+	} );
+}
