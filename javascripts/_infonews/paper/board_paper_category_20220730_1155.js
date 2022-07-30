d89576273ea2717ab6f9e907e8c16f167686aefc
Jaeseong Yoo <praster1@gmail.com>
2022-07-30 11:55:59 +0900

    paper_20220730_1155

---
+++ javascripts/_infonews/paper/board_paper_category_20220730_1155.js
@@ -0,0 +1,31 @@
+function board_paper_category()
+{
+	var table = $('#board_paper_category').DataTable( {
+		"responsive": true,
+		"processing": true,
+		"serverSide": true,
+		"ordering": false,
+		"responsive": true,
+		"autoWidth": true,
+		"searching": false,
+		"paging": false,
+		"ajax": {
+			"url": "/paper/get_paper_category/",
+			"type": "GET",
+			"dataSrc": function(res) {
+				var data = res.data;
+				return data;
+			}
+		},
+		"columns" : [
+			{"data": "Category"}
+		],
+		"oLanguage": { "sInfo" : "" }
+	} );
+
+	table.on( 'responsive-resize', function ( e, datatable, columns ) {
+		var count = columns.reduce( function (a,b) {
+			return b === false ? a+1 : a;
+		}, 0 );
+	} );
+}
