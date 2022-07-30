d80505ad3e0e997e6d999f0530a796f9849019ff
Jaeseong Yoo <praster1@gmail.com>
2022-07-30 17:57:36 +0900

    printTableDataRecentlyUploaded_20220730_1757

---
+++ javascripts/_data/statistics/printTableDataRecentlyUploaded_20220730_1757.js
@@ -0,0 +1,42 @@
+function printTableDataRecentlyUploaded()
+{		
+	var url = "/data_statistics/get_data_recently_uploaded/"
+
+	var table = $('#board_data_recently_uploaded').DataTable( {
+		"destroy": true,
+		"responsive": true,
+		"processing": true,
+		"serverSide": true,
+		"ordering": false,
+		"responsive": true,
+		"autoWidth": true,
+		"paging": false,
+		"pageLength": 5,
+		"lengthChange": false,
+		"searching": false,
+		"ajax": {
+			"url": url,
+			"type": "GET",
+			"dataSrc": function(res) {
+				var data = res.data;
+				return data;
+			}
+		},
+		"columns" : [
+			{"data": "Dataset"}
+		],
+		"oLanguage": { "sInfo" : "" },
+		"language": { "lengthMenu": "",
+						   "processing": "<img src='https://cdn.jsdelivr.net/gh/praster1/statground_cdn/images/tati/9.png' width='35%'/>"
+						},
+		"drawCallback": function( settings ) {
+			 $("#board_data_recently_uploaded thead").remove();
+		 }
+	} );
+	
+	table.on( 'responsive-resize', function ( e, datatable, columns ) {
+		var count = columns.reduce( function (a,b) {
+			return b === false ? a+1 : a;
+		}, 0 );
+	} );
+};
