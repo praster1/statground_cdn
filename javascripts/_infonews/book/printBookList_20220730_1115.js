57bbff7a2659c7016f427fcf36994bba112dd734
Jaeseong Yoo <praster1@gmail.com>
2022-07-30 11:16:05 +0900

    book_20220730_1115

---
+++ javascripts/_infonews/book/printBookList_20220730_1115.js
@@ -0,0 +1,42 @@
+function printBookList(searchText)
+{		
+	var url = "/book/get_board_book_searchResult/"
+	
+	if (searchText != null)
+	{
+		url = url + searchText + "/"
+	}
+
+	var table = $('#board_book_searchResult').DataTable( {
+		"destroy": true,
+		"responsive": true,
+		"processing": true,
+		"serverSide": true,
+		"ordering": false,
+		"responsive": true,
+		"autoWidth": true,
+		"paging": true,
+		"lengthMenu": [ 5, 10, 15, 20, 25 ],
+		"pageLength": 5,
+		"searching": false,
+		"ajax": {
+			"url": url,
+			"type": "GET",
+			"dataSrc": function(res) {
+				var data = res.data;
+				return data;
+			}
+		},
+		"columns" : [
+			{"data": "Book"}
+		],
+		"oLanguage": { "sInfo" : "_TOTAL_개의 도서 중, _START_ ~ _END_  번째 도서" },
+		"language": { "lengthMenu": "페이지당 _MENU_ 개씩 보기" }
+	} );
+	
+	table.on( 'responsive-resize', function ( e, datatable, columns ) {
+		var count = columns.reduce( function (a,b) {
+			return b === false ? a+1 : a;
+		}, 0 );
+	} );		
+};
