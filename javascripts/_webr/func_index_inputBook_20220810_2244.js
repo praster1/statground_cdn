1dec40b3164ec7a6c2d8a8837c1d5d40b9dc2dbe
Jaeseong Yoo <praster1@gmail.com>
2022-08-10 22:44:42 +0900

    func_index_inputBook_20220810_2244

---
+++ javascripts/_webr/func_index_inputBook_20220810_2244.js
@@ -0,0 +1,28 @@
+function func_index_inputBook() {
+	var url = "/webr/get_webr_index_book/?videoCount=1"
+	
+	$.ajax({
+		url : url,
+		success:function(data){
+			
+			for(key in data){
+				$("#index_book").html("")
+				
+				html = '<center>'
+				
+				html += '<a class="thumb-image" href="' + data[key].link + '" target="_blank">'
+				html += '<img src="' + data[key].image + '" class="img-responsive" alt="' + textLengthOverCut(data[key].title, 15) + '" style="height:150px">'
+				html += '</a>'
+				
+				html += '<h5 class="mg-title text-semibold" style="color:black;">' + data[key].title + '</h5>'
+				html += '</center>'
+				
+				html += '<div class="mg-description">'
+				html += '<p>' + textLengthOverCut(data[key].description.toString(), 150) + '</p>'
+				html += '</div>'
+				
+				$("#index_book").append(html)
+			}
+		}
+	})
+}
