8d2cc50f1543845161cb58a300a68e616eaa402a
Jaeseong Yoo <praster1@gmail.com>
2022-08-24 23:15:31 +0900

    printTableDataList_20220824_2313

---
+++ javascripts/_data/data_list/printTableDataList_20220824_2313.js
@@ -0,0 +1,43 @@
+function printTableDataList(categoryUUID)
+{		
+	var url = "/data/get_data_list/"
+	
+	if (categoryUUID != null)
+	{
+		url = url + categoryUUID + "/"
+	}
+
+	var table = $('#board_data_list').DataTable( {
+		"destroy": true,
+		"responsive": true,
+		"processing": true,
+		"serverSide": true,
+		"ordering": false,
+		"responsive": true,
+		"autoWidth": true,
+		"paging": true,
+		"lengthMenu": [ 5, 10, 15, 20, 25 ],
+		"pageLength": 10,
+		"ajax": {
+			"url": url,
+			"type": "GET",
+			"dataSrc": function(res) {
+				var data = res.data;
+				return data;
+			}
+		},
+		"columns" : [
+			{"data": "Dataset"}
+		],
+		"oLanguage": { "sInfo" : "_TOTAL_개의 데이터 중, _START_ ~ _END_  번째 데이터" },
+		"language": { "lengthMenu": "페이지당 _MENU_ 개씩 보기",
+						   "processing": "<img src='https://cdn.jsdelivr.net/gh/praster1/statground_cdn/images/tati/9.png' width='35%'/>"
+						}
+	} );
+	
+	table.on( 'responsive-resize', function ( e, datatable, columns ) {
+		var count = columns.reduce( function (a,b) {
+			return b === false ? a+1 : a;
+		}, 0 );
+	} );
+};
