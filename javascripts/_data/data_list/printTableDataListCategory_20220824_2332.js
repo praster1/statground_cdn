9cc0dda7a0b373c2db3a4e9b1f8f756def6045f0
Jaeseong Yoo <praster1@gmail.com>
2022-08-24 23:33:38 +0900

    printTableDataListCategory_20220824_2332

---
+++ javascripts/_data/data_list/printTableDataListCategory_20220824_2332.js
@@ -0,0 +1,35 @@
+function printTableDataListCategory()
+{
+	var table = $('#board_data_list_category').DataTable( {
+		"responsive": true,
+		"processing": true,
+		"serverSide": true,
+		"ordering": false,
+		"responsive": true,
+		"autoWidth": true,
+		"searching": false,
+		"paging": true,
+		"pagingType": "simple",
+		"lengthChange": false,
+		"pageLength": 12,
+		"ajax": {
+			"url": "/data/get_data_list_category/",
+			"type": "GET",
+			"dataSrc": function(res) {
+				var data = res.data;
+				return data;
+			}
+		},
+		"columns" : [
+			{"data": "Category"}
+		],
+		"oLanguage": { "sInfo" : "_TOTAL_개의 카테고리가 검색되었습니다." },
+		"language": { "processing": "<img src='https://cdn.jsdelivr.net/gh/praster1/statground_cdn/images/tati/9.png' width='35%'/>"}
+	} );
+
+	table.on( 'responsive-resize', function ( e, datatable, columns ) {
+		var count = columns.reduce( function (a,b) {
+			return b === false ? a+1 : a;
+		}, 0 );
+	} );
+}
